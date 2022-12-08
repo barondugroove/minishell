e92c3d48f1215d3f9d746584bd19bdf47f77cad8
Benjamin Chabot <bchabot@3F5.42angouleme.fr>
2022-12-08 18:23:28 +0100

    la branche test

--- srcs/parsing/environment_variables.c
+++ srcs/parsing/environment_variables.c
@@ -6,18 +6,29 @@
 /*   By: bchabot <bchabot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 17:26:43 by bchabot           #+#    #+#             */
-/*   Updated: 2022/12/07 17:43:41 by bchabot          ###   ########.fr       */
+/*   Updated: 2022/12/08 18:14:16 by bchabot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+void add_env_var(t_env_node *env)
+{
+	(void)env;
+}
+
 void	init_env(char **envp)
 {
-//	t_env	env;
+	t_env_node	env;
 	int	i;
 
 	i = 0;
 	while (envp[i])
-		printf("%s\n", envp[i++]);
+	{
+		env.key = ft_strtok(envp[i], "=");
+		env.value = ft_strtok(envp[i], "\0");
+		printf("%s\n", env.key);
+		printf("%s\n", env.value);
+		i++;
+	}
 }
