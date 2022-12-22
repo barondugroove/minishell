c424f3049696c0f77a718320226a925519d1f364
Romain Laforge <rlaforge@3G6.42angouleme.fr>
2022-12-22 18:57:29 +0100

    Push un peu deg mais tranquille ca reste pas longtemps

--- srcs/parsing/parser.c
+++ srcs/parsing/parser.c
@@ -6,23 +6,54 @@
 /*   By: rlaforge <rlaforge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 17:36:49 by bchabot           #+#    #+#             */
-/*   Updated: 2022/12/22 01:20:35 by rlaforge         ###   ########.fr       */
+/*   Updated: 2022/12/22 18:05:21 by rlaforge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+void	remove_quotes(t_tok *tok_head, char *str)
+{
+	int		len;
+	int		start;
+	char	quote;
+
+	len = 0;
+	start = 0;
+	quote = '\0';
+	while (str && str[len])
+	{
+		if ((str[len] == '"' || str[len] == '\'') && quote == '\0')
+		{
+			start = len;
+			quote = str[len];
+		}
+		else if (str[len] == quote && quote != '\0')
+		{
+			quote = '\0';
+			newtoken_back(&tok_head, ft_substr(str, start + 1, len - 1), \
+				ft_strdup(K_DQUO));
+			len++;
+		}
+		len++;
+	}
+
+
+
+	/*
+	if (str[0] == '"')
+		newtoken_back(&tok_head, ft_substr(str, 1, ft_strlen(str) - 2), \
+		ft_strdup(K_DQUO));
+	else if (str[0] == '\'')
+		newtoken_back(&tok_head, ft_substr(str, 1, ft_strlen(str) - 2), \
+		ft_strdup(K_QUO));
+	*/
+}
+
 void	add_token(t_tok *tok_head, char *str)
 {
 	if (str[0] == '"' || str[0] == '\'')
-	{
-		if (str[0] == '"')
-			newtoken_back(&tok_head, ft_substr(str, 1, ft_strlen(str) - 2), \
-			ft_strdup(K_DQUO));
-		else if (str[0] == '\'')
-			newtoken_back(&tok_head, ft_substr(str, 1, ft_strlen(str) - 2), \
-			ft_strdup(K_QUO));
-	}
+		remove_quotes(tok_head, str);
 	else if (ft_strchr(str, '|'))
 		add_pipe_token(tok_head, str);
 	else
