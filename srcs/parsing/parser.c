8b461b45968d7d08f0a45b9cccf0da43b823ecb5
Shisoo64 <shisoo64@gmail.com>
2022-12-23 17:14:42 +0100

    Parsing works good

--- srcs/parsing/parser.c
+++ srcs/parsing/parser.c
@@ -6,53 +6,103 @@
 /*   By: rlaforge <rlaforge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 17:36:49 by bchabot           #+#    #+#             */
-/*   Updated: 2022/12/22 18:05:21 by rlaforge         ###   ########.fr       */
+/*   Updated: 2022/12/23 17:12:55 by rlaforge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	remove_quotes(t_tok *tok_head, char *str)
+/*
+void	remove_quotes(t_tok *tok_head, char *tok)
 {
-	int		len;
-	int		start;
-	char	quote;
+// Sépare les quotes.
+	char	*str;
+	char	*buf;
 
-	len = 0;
-	start = 0;
-	quote = '\0';
-	while (str && str[len])
+	buf = ft_strdup(tok);
+	str = ft_strtok(buf, "\"'");
+	while (str)
 	{
-		if ((str[len] == '"' || str[len] == '\'') && quote == '\0')
-		{
-			start = len;
-			quote = str[len];
-		}
-		else if (str[len] == quote && quote != '\0')
-		{
-			quote = '\0';
-			newtoken_back(&tok_head, ft_substr(str, start + 1, len - 1), \
+		if (*str)
+			newtoken_back(&tok_head, ft_strdup(str), \
 				ft_strdup(K_DQUO));
-			len++;
-		}
-		len++;
+		str = ft_strtok(NULL, "\"'");
 	}
+	free(buf);
+	
 
-
-
-	/*
+// Retire les quotes du début et de la fin.
 	if (str[0] == '"')
 		newtoken_back(&tok_head, ft_substr(str, 1, ft_strlen(str) - 2), \
 		ft_strdup(K_DQUO));
 	else if (str[0] == '\'')
 		newtoken_back(&tok_head, ft_substr(str, 1, ft_strlen(str) - 2), \
 		ft_strdup(K_QUO));
-	*/
+
+}
+*/
+
+void	clean_token_list(t_tok *head)
+{
+	t_tok	*tok;
+
+	tok = head;
+	while (tok)
+	{
+		if(*tok->key == '|')
+		{
+			free(tok->next->key);
+			tok->next->key = ft_strdup("C");
+		}
+		tok = tok->next;
+	}
+}
+
+void	remove_quotes(t_tok *tok_head, char *str)
+{
+    int len;
+    int i;
+	int j;
+	char	quote;
+
+	quote = '\0';
+	len = strlen(str);
+	i = 0;
+    while (i < len)
+	{
+        if ((str[i] == '"' || str[i] == '\'') && quote == '\0')
+		{
+			quote = str[i];	
+            j = i;
+            while (j < len - 1) {
+                str[j] = str[j + 1];
+                j++;
+            }
+            str[j] = '\0';
+            len = strlen(str);
+            i--;
+        }
+		else if (str[i] == quote && quote != '\0')
+		{
+			quote = '\0';
+            j = i;
+            while (j < len - 1) {
+                str[j] = str[j + 1];
+                j++;
+            }
+            str[j] = '\0';
+            len = strlen(str);
+            i--;
+        }
+        i++;
+    }
+	newtoken_back(&tok_head, ft_strdup(str), \
+		ft_strdup(K_ARG));
 }
 
 void	add_token(t_tok *tok_head, char *str)
 {
-	if (str[0] == '"' || str[0] == '\'')
+	if (ft_strchr(str, '"') || ft_strchr(str, '\''))
 		remove_quotes(tok_head, str);
 	else if (ft_strchr(str, '|'))
 		add_pipe_token(tok_head, str);
@@ -81,7 +131,8 @@ t_tok	*parser(char *prompt)
 		if (str && *str)
 			add_token(tok_head, str);
 	}
+	clean_token_list(tok_head);
 	return (tok_head);
 }
 
-// parse test| ls -la |grep "c'est trop cool" | wc -l | echo "voici un pipe : |"  mais il est entre '"' du coup il est pas pi|pe.
+// parse test| ls -la |grep "c'est trop cool" | wc -l | e"c"ho "voici un pipe : |"  mais il est entre '"' du coup il est pas pi|pe.
