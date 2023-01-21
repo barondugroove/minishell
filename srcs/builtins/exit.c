9c4629b44bbc5c4145223215251d1e476421f196
barondugroove <chabot.benjamin@gmail.com>
2023-01-21 17:43:06 +0000

    redirections start

---
+++ srcs/builtins/exit.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: benjaminchabot <benjaminchabot@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/19 16:17:14 by benjamincha       #+#    #+#             */
+/*   Updated: 2023/01/21 18:21:14 by benjamincha      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void	ft_exit(long long code)
+{
+	exit_code = code;
+	exit(code);
+}
+
+long long		ft_atoll(const char *str)
+{
+	int			i;
+	int			neg;
+	long long	nbr;
+
+	i = 0;
+	neg = 1;
+	nbr = 0;
+	while (str[i] <= 32)
+		++i;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			neg = -1;
+		++i;
+	}
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		nbr = nbr * 10 + (str[i] - '0');
+		++i;
+	}
+	return (neg * nbr);
+}
+
+static void numeric_argument_required(char *arg)
+{
+	ft_putstr_fd("minishell: exit: ", 2);
+	ft_putstr_fd(arg, 2);
+	ft_putstr_fd(": numeric argument required\n", 2);
+	return ;
+}
+
+int check_args(char **args)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (args[i])
+		i++;
+	if (i > 2)
+	{
+		ft_putstr_fd("exit\n", 2);
+		ft_putstr_fd("minishell: exit: too many arguments", 2);
+		ft_exit(1);
+	}
+	i = 1;
+	while (args[i])
+	{
+		j = 0;
+		while (args[i][j])
+		{
+			if (!ft_isdigit(args[i][j]) && (args[i][j] != '+' && args[i][j] != '-'))
+			{
+				numeric_argument_required(args[i]);
+				ft_exit(2);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	exit_builtin(char **args)
+{
+    long long	arg_nbr;
+
+	exit_code = 0;
+	if (!args[1])
+		ft_exit(0);
+	check_args(args);
+	arg_nbr = ft_atoll(args[1]);
+	if (arg_nbr > INT_MAX || arg_nbr < INT_MIN)
+		ft_exit(1);
+	else
+		ft_exit(arg_nbr);
+	return (0);
+}
---
+++ srcs/execution/redirections.c
@@ -0,0 +1,142 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirections.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: benjaminchabot <benjaminchabot@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/19 13:45:58 by benjamincha       #+#    #+#             */
+/*   Updated: 2023/01/21 17:18:57 by benjamincha      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+/***************************************************\
+||                                                 ||
+||   Need to handle multiple redir like :          ||
+||    - ls > lol >> lol2                           ||
+||    creates both files but only fill lol2        ||
+||                                                 ||
+||   Need to handle here_doc                       ||
+||   Need to handle multiple file args like :      ||
+||    - cat <./files/infile_big ./files/infile     ||
+||                                                 ||
+\***************************************************/
+
+
+void	error_file(char *file, int errnum)
+{
+	ft_putstr_fd(strerror(errnum), 2);
+	ft_putstr_fd(": ", 2);
+	ft_putendl_fd(file, 2);
+}
+
+char    *get_file(t_tok *cmds)
+{
+    t_tok   *tmp;
+
+    tmp = cmds;
+    while (tmp->next)
+    {
+        if (*tmp->key == '>' && *tmp->next->key == '>')
+            return (tmp->next->next->value);
+        else if (*tmp->key == '>' || *tmp->key == '<')
+            return (tmp->next->value); 
+        tmp = tmp->next;
+    }
+    return (NULL);
+}
+
+int has_redir(t_tok *cmds)
+{
+    t_tok   *tmp;
+
+    tmp = cmds;
+    while (tmp->next && *tmp->key != '|')
+    {
+        if (*tmp->key == '>' && *tmp->next->key == '>')
+            return (3);
+        else if (*tmp->key == '>')
+            return (1);
+        else if  (*tmp->key == '<')
+            return (2);
+        tmp = tmp->next;
+    }
+    return (0);
+}
+
+int redir_nbr(t_tok *cmds)
+{
+    t_tok   *tmp;
+    int     i;
+
+    tmp = cmds;
+    i = 0;
+    while (tmp->next && *tmp->key != '|')
+    {
+        if (*tmp->key == '>' && *tmp->next->key == '>')
+        {
+            i++;
+            tmp = tmp->next;
+        }        
+        else if (*tmp->key == '>')
+            i++;
+        else if  (*tmp->key == '<')
+            i++;
+        tmp = tmp->next;
+    }
+    return (i);
+}
+
+int check_file(char *file, int dir)
+{
+    if (dir == 0 && access(file, F_OK && R_OK) == 0)
+        return (0);
+    else if (dir == 1 && access(file, F_OK && W_OK) == 0)
+        return (0);
+    else
+    {
+    	ft_putstr_fd("minishell: ", 2);
+	    ft_putstr_fd(file, 2);
+	    ft_putstr_fd(": No such file or directory\n", 2);
+	    return (1);
+    }
+}
+
+void handle_redirection(t_tok *cmds)
+{
+    int 	fd_in = -1;
+    int 	fd_out = -1;
+    int     nbr;
+	char	*str;
+	
+	str = get_file(cmds);
+    nbr = redir_nbr(cmds);
+    if (!str)
+    {
+        return ;
+    }
+	if (has_redir(cmds) == 1)
+	{
+        fd_out = open(str, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+		dup2(fd_out, 1);
+    }
+	if (has_redir(cmds) == 3)
+    {
+        fd_out = open(str, O_CREAT | O_WRONLY | O_APPEND, 0644);
+        dup2(fd_out, 1);
+    }
+    if (has_redir(cmds) == 2 && !check_file(str, 0))
+    {
+        fd_in = open(str, O_RDONLY);
+        dup2(fd_in, 0);
+    }
+    if (--nbr > 0)
+
+        handle_redirection(cmds->next);
+    if (fd_in != -1)
+        close(fd_in);
+    if (fd_out != -1)
+        close(fd_out);
+}
